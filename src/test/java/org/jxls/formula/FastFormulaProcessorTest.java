de56828afb32e38716f3b255733d295792d04b5f
Michal Kepkowski <michal.kepkowski@sagiton.pl>
2018-02-23 17:11:54 +0100

    Strict regex cell name + test

---
+++ src/test/java/org/jxls/formula/FastFormulaProcessorTest.java
@@ -0,0 +1,68 @@
+package org.jxls.formula;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.jxls.common.CellData;
+import org.jxls.common.CellRef;
+import org.jxls.transform.Transformer;
+import org.jxls.util.TransformerFactory;
+import org.mockito.ArgumentCaptor;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
+/**
+ * @author Michał Kępkowski
+ */
+public class FastFormulaProcessorTest {
+    @Test
+    public void FastFormulaProcessorIfFormulaTest() throws IOException {
+        //BEFORE
+        InputStream template = getTestSheet();
+        OutputStream outputStream = new ByteArrayOutputStream();
+        Transformer transformer = spy(getTransformer(template, outputStream));
+        ArgumentCaptor<String> firstFooCaptor = ArgumentCaptor.forClass(String.class);
+        String expected = "IF(AE201=0.0,\"\",AE211/AE201)";
+
+        //TEST
+        FastFormulaProcessor fastFormulaProcessor = new FastFormulaProcessor();
+        fastFormulaProcessor.processAreaFormulas(transformer);
+
+        //ASSERT
+        verify(transformer).setFormula(any(CellRef.class),firstFooCaptor.capture());
+        Assert.assertEquals(expected,firstFooCaptor.getAllValues().get(0));
+    }
+
+    private Transformer getTransformer(InputStream template, OutputStream outputStream) {
+        Transformer transformer = TransformerFactory.createTransformer(template, outputStream);
+        transformer.getTargetCellRef(new CellRef("Arkusz1",20,4))
+                .add(new CellRef("Arkusz1",200,30));
+        transformer.getTargetCellRef(new CellRef("Arkusz1",21,4))
+                .add(new CellRef("Arkusz1",210,30));
+
+        List<CellData> cellDataList = new ArrayList(transformer.getFormulaCells());
+        getIFFormula(cellDataList)
+                .addTargetPos(new CellRef("Arkusz1",12,12));
+        return transformer;
+    }
+
+    private CellData getIFFormula(List<CellData> cellData){
+        for(CellData cell : cellData){
+            if(cell.getFormula().startsWith("IF")){
+                return cell;
+            }
+        }
+        return null;
+    }
+
+    private InputStream getTestSheet() {
+        return getClass().getClassLoader()
+                .getResourceAsStream("org/jxls/formula/test_formula.xls");
+    }
+}
