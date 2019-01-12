3425fd381aba60a2a6ce63396eccaebaf32999c7
zang.loo <zhanglum@139.com>
2019-01-13 07:26:43 +0800

    rename multisheet sheet name on exists with pattern like "sheetName(i)"

--- src/main/java/org/jxls/command/DynamicSheetNameGenerator.java
+++ src/main/java/org/jxls/command/DynamicSheetNameGenerator.java
@@ -4,11 +4,15 @@ import org.jxls.common.CellRef;
 import org.jxls.common.Context;
 import org.jxls.expression.ExpressionEvaluator;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Creates cell references based on passed sheet names
  */
 public class DynamicSheetNameGenerator implements CellRefGenerator {
     private String sheetName;
+    private Set<String> names = new HashSet<String>();
     private CellRef startCellRef;
     private ExpressionEvaluator expressionEvaluator;
 
@@ -23,6 +27,18 @@ public class DynamicSheetNameGenerator implements CellRefGenerator {
         String name = (String)expressionEvaluator.evaluate(sheetName, context.toMap());
         if (name == null) {
             return null;
+        }
+        if (names.contains(name)) {
+            String tmp;
+            for (int i = 1;;i++) {
+                tmp = name+'(' + i + ')';
+                if (names.add(tmp)) {
+                    break;
+                }
+            }
+            name = tmp;
+        } else {
+            names.add(name);
         }
         return new CellRef(name, startCellRef.getRow(), startCellRef.getCol());
     }
