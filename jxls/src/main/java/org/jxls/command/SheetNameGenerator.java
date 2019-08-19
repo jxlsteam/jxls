85c2d54fd350caa2994c2c29bd634dcb26b82ca3
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2019-08-19 11:51:42 +0200

    Fix issue 161
    I introduced a new dependency to apache poi here for sake of name
    sanitization. Hopefully this is OK.

--- jxls/src/main/java/org/jxls/command/SheetNameGenerator.java
+++ jxls/src/main/java/org/jxls/command/SheetNameGenerator.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.jxls.common.CellRef;
 import org.jxls.common.Context;
+import org.apache.poi.ss.util.WorkbookUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Creates cell references based on passed sheet names
@@ -11,6 +14,7 @@ import org.jxls.common.Context;
 public class SheetNameGenerator implements CellRefGenerator {
     private final List<String> sheetNames;
     private final CellRef startCellRef;
+    private static Logger logger = LoggerFactory.getLogger(SheetNameGenerator.class);
 
     public SheetNameGenerator(List<String> sheetNames, CellRef startCellRef) {
         this.sheetNames = sheetNames;
@@ -22,7 +26,12 @@ public class SheetNameGenerator implements CellRefGenerator {
         if (sheetNames.size() <= index) {
             return null;
         }
-        String sheetName = sheetNames.get(index);
+
+        String givenSheetName = sheetNames.get(index);
+        String sheetName = WorkbookUtil.createSafeSheetName(givenSheetName);
+        if (givenSheetName != sheetName) {
+            logger.warn("Given sheet name was invalid, updating. originalSheetName={} newSheetName={}", givenSheetName, sheetName);
+        }
         return new CellRef(sheetName, startCellRef.getRow(), startCellRef.getCol());
     }
 }
