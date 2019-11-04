be1c3352f56e1b8e866a9f02dd0de4207fa5cba1
Marcus Warm <warm@mwvb.de>
2019-11-04 22:49:44 +0100

    Changed use of PoiTestHelper to use of existing TestWorkbook class

--- jxls-poi/src/test/java/org/jxls/transform/poi/ConditionalFormattingTest.java
+++ jxls-poi/src/test/java/org/jxls/transform/poi/ConditionalFormattingTest.java
@@ -1,14 +1,6 @@
 package org.jxls.transform.poi;
 
-import org.apache.poi.ss.usermodel.ConditionalFormatting;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.SheetConditionalFormatting;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
-import org.apache.poi.ss.util.CellRangeAddress;
-import org.junit.Test;
-import org.jxls.common.Context;
-import org.jxls.util.JxlsHelper;
+import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -17,15 +9,21 @@ import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import org.apache.poi.ss.usermodel.ConditionalFormatting;
+import org.apache.poi.ss.usermodel.SheetConditionalFormatting;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.junit.Test;
+import org.jxls.command.TestWorkbook;
+import org.jxls.common.Context;
+import org.jxls.util.JxlsHelper;
 
 public class ConditionalFormattingTest {
-
     private static final double EPSILON = 0.001;
 
     @Test
     public void shouldCopyConditionalFormatInEachCommandLoop() throws IOException {
-        InputStream is = ConditionalFormattingTest.class.getResourceAsStream("cond_format_template.xlsx");
+        // Prepare
+        InputStream is = getClass().getResourceAsStream("cond_format_template.xlsx");
         String outputFileName = "target/cond_format_output.xlsx";
         File outputFile = new File(outputFileName);
         FileOutputStream out = new FileOutputStream(outputFile);
@@ -34,21 +32,28 @@ public class ConditionalFormattingTest {
         context.putVar("numbers", list);
         context.putVar("val1", 0);
         context.putVar("val2", 7);
+        
+        // Test
         JxlsHelper.getInstance().processTemplate(is, out, context);
-        Workbook workbook = WorkbookFactory.create(outputFile);
-        for(int i = 0; i < list.size(); i++){
-            assertEquals(list.get(i).doubleValue(), PoiTestHelper.cellNumericValue(workbook, i + 1, 1), EPSILON);
-        }
-        Sheet sheet = workbook.getSheetAt(0);
-        SheetConditionalFormatting sheetConditionalFormatting = sheet.getSheetConditionalFormatting();
-        int conditionalFormattingCount = 0;
-        for( int i = 0; i < sheetConditionalFormatting.getNumConditionalFormattings(); i++){
-            ConditionalFormatting conditionalFormatting = sheetConditionalFormatting.getConditionalFormattingAt(i);
-            CellRangeAddress[] ranges = conditionalFormatting.getFormattingRanges();
-            if (ranges.length > 0){
-                conditionalFormattingCount++;
+
+        // Verify
+        try (TestWorkbook xls = new TestWorkbook(outputFile)) {
+            xls.selectSheet(0);
+            for (int i = 0; i < list.size(); i++) {
+                double val = xls.getCellValueAsDouble(i + 2, 2);
+                assertEquals(list.get(i).doubleValue(), val, EPSILON);
             }
+
+            SheetConditionalFormatting sheetConditionalFormatting = xls.getSheetConditionalFormatting();
+            int conditionalFormattingCount = 0;
+            for (int i = 0; i < sheetConditionalFormatting.getNumConditionalFormattings(); i++) {
+                ConditionalFormatting conditionalFormatting = sheetConditionalFormatting.getConditionalFormattingAt(i);
+                CellRangeAddress[] ranges = conditionalFormatting.getFormattingRanges();
+                if (ranges.length > 0) {
+                    conditionalFormattingCount++;
+                }
+            }
+            assertEquals(list.size() + 2, conditionalFormattingCount);
         }
-        assertEquals(list.size() + 2, conditionalFormattingCount);
     }
 }
