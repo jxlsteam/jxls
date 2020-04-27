21cecf9c1f4fe4ab93c9b56da368bce2f2ccef3e
Marcus Warm <warm@mwvb.de>
2020-04-27 08:34:27 +0200

    #17 image and multi sheet

---
+++ jxls-poi/src/test/java/org/jxls/templatebasedtests/IssueG17Test.java
@@ -0,0 +1,79 @@
+package org.jxls.templatebasedtests;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.jxls.JxlsTester;
+import org.jxls.common.Context;
+
+/**
+ * Github issue 17: Image and multi sheet
+ */
+public class IssueG17Test {
+
+    @Test
+    public void test() throws Exception {
+        // Prepare
+        Context context = new Context();
+        context.putVar("sheets", getItems());
+        
+        // Test
+        JxlsTester tester = JxlsTester.xlsx(getClass());
+        // with evaluate formulas = true
+        tester.processTemplate(context);
+        
+        // Verify
+        // Each sheet with an image?
+    }
+
+    private List<Item> getItems() {
+        List<Item> items = new ArrayList<>();
+        Item item = new Item();
+        item.setId("registered_user");
+        item.setCountry_code("JP");
+        item.setApp_type("ANDROID");
+        items.add(item);
+        item = new Item();
+        item.setId("registered_user1");
+        item.setCountry_code("KO");
+        item.setApp_type("ANDROID");
+        items.add(item);
+        item = new Item();
+        item.setId("registered_user2");
+        item.setCountry_code("US");
+        item.setApp_type("ANDROID");
+        items.add(item);
+        return items;
+    }
+
+    public static class Item {
+        private String id;
+        private String country_code;
+        private String app_type;
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getCountry_code() {
+            return country_code;
+        }
+
+        public void setCountry_code(String country_code) {
+            this.country_code = country_code;
+        }
+
+        public String getApp_type() {
+            return app_type;
+        }
+
+        public void setApp_type(String app_type) {
+            this.app_type = app_type;
+        }
+    }    
+}
