2be96bd5597a4055178b73a37ae2cf023a886b6c
Marcus Warm <warm@mwvb.de>
2021-02-17 17:05:45 +0100

    #79 code-review

--- jxls/src/main/java/org/jxls/extractors/LiteralsExtractor.java
+++ jxls/src/main/java/org/jxls/extractors/LiteralsExtractor.java
@@ -2,6 +2,7 @@ package org.jxls.extractors;
 
 import static java.util.stream.Collectors.toList;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Stack;
@@ -9,16 +10,10 @@ import java.util.Stack;
 import org.jxls.builder.xls.XlsCommentAreaBuilder;
 
 public class LiteralsExtractor {
-    public static final int COMMAND_PREFIX_LENGTH = 3;
-    private String text;
-    private List<String> literalList;
+    private static final int COMMAND_PREFIX_LENGTH = XlsCommentAreaBuilder.COMMAND_PREFIX.length();
     
-    public LiteralsExtractor(String text, List<String> literalList) {
-        this.literalList = literalList;
-        this.text = text;
-    }
-    
-    public List<String> extract() {
+    public List<String> extract(final String text) {
+        List<String> literalList = new ArrayList<>();
         String literal = "";
         for (int i = 0, n = text.length(); i < n; i++) {
             if (literal.length() >= COMMAND_PREFIX_LENGTH && literal.endsWith(XlsCommentAreaBuilder.COMMAND_PREFIX)) {
@@ -30,19 +25,17 @@ public class LiteralsExtractor {
                 String cmd = makeOperators(remainingText);
                 literal = "";
                 literalList.add(cmd);
-                i = i + cmd.length() - COMMAND_PREFIX_LENGTH;
+                i += cmd.length() - COMMAND_PREFIX_LENGTH;
 
                 if (i != n) {
                     // if text no ending
                     literal = String.valueOf(text.charAt(i));                    
-                }
-                else {
+                } else {
                     literal = "";                    
                 }
-
             } else {
                 // else is the next char to new literal
-                literal = literal + String.valueOf(text.charAt(i));
+                literal += String.valueOf(text.charAt(i));
             }
         }
 
@@ -56,57 +49,50 @@ public class LiteralsExtractor {
     private void makeCommentLines(String literal, List<String> literalList) {
         literalList.addAll(
                 Arrays.stream(literal.split("\n"))
-                    .filter(line -> ! line.isEmpty())
+                    .filter(line -> !line.isEmpty())
                     .collect(toList()));
     }
     
-    private String makeOperators(String text) {
-        // result command
-        String cmd = "";
+    private String makeOperators(final String text) {
+        String cmd = ""; // result command
         Stack<Character> bracketsStack = new Stack<>();
         Stack<Character> quotationsStack = new Stack<>();
 
         for (int i = 0; i < text.length(); i++) {
             char symbol = text.charAt(i);
 
-            if (symbol == '"' || symbol == '\'') {
-                if (!quotationsStack.empty() && quotationsStack.peek() == symbol) {
-                    quotationsStack.pop();                    
-                }
-                else {
-                    quotationsStack.push(symbol);                                    
-                }
-            }
-
             switch (symbol) {
-            case '(':
-                if (quotationsStack.empty()) {
-                    bracketsStack.push(symbol);                    
-                }
-                break;
-            case ')':
-                if (quotationsStack.empty()) {
-                    if (bracketsStack.peek() == '(') {
-                        bracketsStack.pop();
-                        if (bracketsStack.empty()) {
-                            return cmd + symbol;                            
-                        }
+                case '"':
+                case '\'':
+                    if (!quotationsStack.empty() && quotationsStack.peek() == symbol) {
+                        quotationsStack.pop();                    
                     } else {
-                        // no opening, but closing !!
-                        String errorMessage = "Got closing ) but no opening of it. Pos: " + text.substring(0, i);
-                        throw new IllegalArgumentException(errorMessage);
+                        quotationsStack.push(symbol);                                    
                     }
-                }
-                break;
-            default:
-                break;
+                    break;
+                case '(':
+                    if (quotationsStack.empty()) {
+                        bracketsStack.push(symbol);                    
+                    }
+                    break;
+                case ')':
+                    if (quotationsStack.empty()) {
+                        if (bracketsStack.peek() == '(') {
+                            bracketsStack.pop();
+                            if (bracketsStack.empty()) {
+                                return cmd + symbol;                            
+                            }
+                        } else {
+                            // no opening, but closing !!
+                            String errorMessage = "Got closing ) but no opening of it. Pos: " + text.substring(0, i);
+                            throw new IllegalArgumentException(errorMessage);
+                        }
+                    }
+                    break;
             }
 
-            cmd = cmd + symbol;
-
+            cmd += symbol;
         }
-
         return cmd;
     }
-    
 }
