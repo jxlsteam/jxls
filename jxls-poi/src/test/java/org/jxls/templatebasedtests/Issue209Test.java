37bbf83087e1505c50973f72e5580e9613bc54a3
Marcus Warm <warm@mwvb.de>
2023-07-07 09:20:52 +0200

    issue 209 release notes, testcase for old behavior

--- jxls-poi/src/test/java/org/jxls/templatebasedtests/Issue209Test.java
+++ jxls-poi/src/test/java/org/jxls/templatebasedtests/Issue209Test.java
@@ -9,6 +9,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.jxls.JxlsTester;
 import org.jxls.TestWorkbook;
+import org.jxls.command.EachCommand;
 import org.jxls.common.Context;
 
 /**
@@ -18,7 +19,7 @@ import org.jxls.common.Context;
 public class Issue209Test {
 
     @Test
-    public void test() {
+    public void testNewBehavior() {
         // Prepare
         List<Map<String, String>> employees = new ArrayList<>();
         employees.add(createEmployee("Department A", "Claudia", "Amsterdam")); // First must not be Geldern!
@@ -45,7 +46,41 @@ public class Issue209Test {
             Assert.assertEquals("Geldern", w.getCellValueAsString(8, 3));
         }
     }
-    
+
+    @Test
+    public void testOldBehavior() {
+        EachCommand.oldSelectBehavior = true;
+        try {
+            // Prepare
+            List<Map<String, String>> employees = new ArrayList<>();
+            employees.add(createEmployee("Department A", "Dagmar", "Geldern"));
+            employees.add(createEmployee("Department A", "Sven", "Geldern"));
+            employees.add(createEmployee("Department A", "Claudia", "Amsterdam")); // First must not be Geldern!
+            employees.add(createEmployee("Department B", "Melanie", "Geldern"));
+            employees.add(createEmployee("Department B", "Doris", "Wetten"));
+            employees.add(createEmployee("Department C", "Stefan", "Bruegge"));
+            Context context = new Context();
+            context.putVar("employees", employees);
+            
+            // Test
+            JxlsTester tester = JxlsTester.xlsx(getClass());
+            tester.processTemplate(context);
+            
+            // Verify
+            try (TestWorkbook w = tester.getWorkbook()) {
+                w.selectSheet(0);
+                Assert.assertEquals("Department A", w.getCellValueAsString(3, 1));
+                Assert.assertEquals("Dagmar", w.getCellValueAsString(4, 2));
+                Assert.assertEquals("Sven", w.getCellValueAsString(5, 2));
+                Assert.assertEquals("Department B", w.getCellValueAsString(7, 1));
+                Assert.assertEquals("Melanie", w.getCellValueAsString(8, 2));
+                Assert.assertEquals("Geldern", w.getCellValueAsString(8, 3));
+            }
+        } finally {
+            EachCommand.oldSelectBehavior = false;
+        }
+    }
+
     private Map<String, String> createEmployee(String department, String name, String city) {
         Map<String, String> map = new HashMap<>();
         map.put("department", department);
