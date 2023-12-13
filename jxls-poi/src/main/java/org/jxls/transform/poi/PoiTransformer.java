e38ad39087feb120677867380aebeb7e9251aa9b
Marcus Warm <marcus.warm@x-map.de>
2023-12-13 13:16:03 +0100

    XDEV-5885 #251 bugfix: columns must be sorted

--- jxls-poi/src/main/java/org/jxls/transform/poi/PoiTransformer.java
+++ jxls-poi/src/main/java/org/jxls/transform/poi/PoiTransformer.java
@@ -539,7 +539,7 @@ public class PoiTransformer extends AbstractTransformer {
                 }
             }
         }
-		commentDataCells.sort((a, b) -> a.getCol() - b.getCol());
+        commentDataCells.sort((a, b) -> a.getCol() - b.getCol());
         return commentDataCells;
     }
 
