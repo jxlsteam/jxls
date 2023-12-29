523188d390c85ba8ca7ff188fa5d9560fe58c990
Marcus Warm <warm@mwvb.de>
2023-12-29 16:40:19 +0100

    create simple PoiTransformer

--- jxls-poi/src/main/java/org/jxls/transform/poi/JxlsPoi.java
+++ jxls-poi/src/main/java/org/jxls/transform/poi/JxlsPoi.java
@@ -1,10 +1,13 @@
 package org.jxls.transform.poi;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Map;
 
 import org.jxls.builder.JxlsStreaming;
+import org.jxls.common.PoiExceptionLogger;
 
 public class JxlsPoi {
 
@@ -16,5 +19,21 @@ public class JxlsPoi {
                 .withStreaming(streaming)
                 .withTemplate(template)
                 .buildAndFill(data, outputFile);
+    }
+
+    public static void fill(InputStream template, JxlsStreaming streaming, Map<String, Object> data, OutputStream out) {
+        JxlsPoiTemplateFillerBuilder.newInstance()
+                .withStreaming(streaming)
+                .withTemplate(template)
+                .buildAndFill(data, () -> out);
+    }
+    
+    /**
+     * Use this method if you just need PoiTransformer functionality for a template file.
+     * @param template -
+     * @return PoiTransformer
+     */
+    public static PoiTransformer createSimple(InputStream template) {
+        return (PoiTransformer) new PoiTransformerFactory().create(template, new ByteArrayOutputStream(), JxlsStreaming.STREAMING_OFF, new PoiExceptionLogger());
     }
 }
