78c6a277fd7222c4a65a7b7ab3bd2e1447883cdd
Marcus Warm <warm@mwvb.de>
2023-12-29 17:45:59 +0100

    isConditionTrue(condition, map)

--- jxls/src/main/java/org/jxls/expression/ExpressionEvaluator.java
+++ jxls/src/main/java/org/jxls/expression/ExpressionEvaluator.java
@@ -19,28 +19,29 @@ public interface ExpressionEvaluator {
     String getExpression();
     
     /**
-     * Evaluates if getExpression() is true
+     * Evaluates if getExpression() is true.
      * @param context data access
      * @return expression result (true or false)
      * @throws JxlsException if return value is not a Boolean or null
      */
     default boolean isConditionTrue(Context context) {
-        return isConditionTrue(context.toMap());
+        return isConditionTrue(getExpression(), context.toMap());
     }
 
     /**
      * Evaluates if getExpression() is true. Call this method only if you have no Context.
+     * @param condition -
      * @param data -
      * @return expression result (true or false)
      * @throws JxlsException if return value is not a Boolean or null
      */
-    default boolean isConditionTrue(Map<String, Object> data) {
-        Object conditionResult = evaluate(data);
+    default boolean isConditionTrue(String condition, Map<String, Object> data) {
+        Object conditionResult = evaluate(condition, data);
         if (conditionResult instanceof Boolean b) {
             return Boolean.TRUE.equals(b);
         } else if (conditionResult == null) {
-            throw new JxlsException("Result of condition \"" + getExpression() + "\" is null");
+            throw new JxlsException("Result of condition \"" + condition + "\" is null");
         }
-        throw new JxlsException("Result of condition \"" + getExpression() + "\" is not a Boolean");
+        throw new JxlsException("Result of condition \"" + condition + "\" is not a Boolean");
     }
 }
